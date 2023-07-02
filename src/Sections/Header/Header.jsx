dbb76d0fff49844044b87473fac032f80502c9cf
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-07-02 19:46:23 +0300

    Fix

---
+++ src/Sections/Header/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import "./Header.scss"
+
+export default function Header() {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="">Service</Link></li>
+                <li><Link to="">Blog</Link></li>
+                <li><Link to="">About</Link></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
---
+++ src/_index.js
@@ -0,0 +1,9 @@
+// Sections
+export { default as Header } from "./Sections/Header/Header";
+export { default as Main } from "./Sections/Main/Main";
+export { default as Footer } from "./Sections/Footer/Footer";
+// Pages
+export { default as Home } from "./Pages/Home/Home";
+// Layout
+export { default as Layout } from "./Layout/Layout";
+
